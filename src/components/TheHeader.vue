93562227310f46c92973464aa03945ba1e58c112
Michiel Koning <mail@michielkoning.nl>
2020-06-13 20:37:04 +0200

    vue project

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <center-wrapper>
+      <h1>Police Abuses</h1>
+      <p>{{ totalVideos }} Videos of Police Brutality From George Floyd Protests</p>
+    </center-wrapper>
+  </header>
+</template>
+
+<script>
+import videos from '@/data/videos.json'
+import CenterWrapper from '@/components/CenterWrapper.vue'
+
+export default {
+  components: {
+    CenterWrapper,
+  },
+  computed: {
+    totalVideos() {
+      const totalVideos = videos.list.filter((video) => video.youtube.length)
+      return totalVideos.length
+    },
+  },
+}
+</script>
+
+<style scoped lang="postcss">
+header {
+  @mixin color-negative;
+
+  text-align: center;
+  padding: 1em 0 0;
+}
+
+p {
+  margin: 0;
+}
+
+a {
+  display: inline-block;
+  margin: 0 0.25em;
+}
+</style>
