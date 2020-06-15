bfca1dfe0f09f30b0bfc88acfe83b605ec65d3b7
Michiel Koning <michiel.koning@valtech.com>
2020-06-15 15:55:34 +0200

    set start url

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   pwa: {
     name: 'Police Abuses',
+    start_url: 'https://deploy-preview-10--police-accountability.netlify.app/',
     themeColor: '#021E48',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
