0abf340744653dcc492ec73d8fa8a49a1c8de0fc
Michiel Koning <mail@michielkoning.nl>
2020-06-16 13:06:02 +0200

    theme color

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pwa: {
     name: 'Police Abuses',
-    themeColor: '#021E48',
-    msTileColor: '#000000',
+    themeColor: '#021e48',
+    msTileColor: '#021e48',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black-translucent',
     workboxPluginMode: 'InjectManifest',
