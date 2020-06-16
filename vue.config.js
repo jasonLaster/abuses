e0fc758a02bae2a184c33ed88f64631080ab70ab
Michiel Koning <mail@michielkoning.nl>
2020-06-16 13:09:09 +0200

    manifest update

--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,5 @@ module.exports = {
     msTileColor: '#021e48',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black-translucent',
-    workboxPluginMode: 'InjectManifest',
-    manifestOptions: {
-      start_url: 'index.html',
-    },
-    workboxOptions: {
-      swSrc: 'service-worker.js',
-    },
   },
 }
