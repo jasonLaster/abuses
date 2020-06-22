ea9f2ba6ca5e5d1ddb9d522aa23dba51c86b1c12
Michiel Koning <michiel.koning@valtech.com>
2020-06-22 12:32:00 +0200

    added routeranimation

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,34 @@
   <div id="app" :class="$style.page">
     <the-header />
     <main :class="$style.main">
-      <router-view />
+      <router-animation :direction="direction">
+        <router-view />
+      </router-animation>
     </main>
   </div>
 </template>
 
 <script>
 import TheHeader from '@/components/TheHeader.vue'
+import RouterAnimation from '@/components/Animations/RouterAnimation.vue'
 
 export default {
   name: 'App',
   components: {
+    RouterAnimation,
     TheHeader,
+  },
+  data() {
+    return {
+      direction: '',
+    }
+  },
+  watch: {
+    // watch the `$route` to determine the transition to use
+    $route(to) {
+      const isListPage = to.name === 'Root' || to.name === 'City'
+      this.direction = isListPage ? 'left' : 'right'
+    },
   },
 }
 </script>
@@ -33,5 +49,6 @@ export default {
   padding-top: 2em;
   flex: 1 1 auto;
   background: var(--color-white);
+  overflow-x: hidden;
 }
 </style>
---
+++ src/components/Animations/RouterAnimation.vue
@@ -0,0 +1,57 @@
+<template>
+  <transition :name="transitionName">
+    <slot />
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    direction: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    transitionName() {
+      return `slide-${this.direction}`
+    },
+  },
+}
+</script>
+
+<style lang="postcss">
+:root {
+  --router-animation-duration: 0.15s;
+  --router-animation-distance: 2em;
+}
+
+.slide-left-enter-active,
+.slide-left-leave-active,
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition: all var(--router-animation-duration);
+}
+
+.slide-left-enter-active,
+.slide-right-enter-active {
+  transition-delay: var(--router-animation-duration);
+}
+
+.slide-left-enter,
+.slide-left-leave-active,
+.slide-right-enter,
+.slide-right-leave-active {
+  opacity: 0;
+}
+
+.slide-right-leave-active,
+.slide-left-enter {
+  transform: translateX(calc(-1 * var(--router-animation-distance)));
+}
+
+.slide-left-leave-active,
+.slide-right-enter {
+  transform: translateX(var(--router-animation-distance));
+}
+</style>
