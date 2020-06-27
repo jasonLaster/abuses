a019596a7a608b7dd1cafecb7f97b617d1e7449c
Michiel Koning <mail@michielkoning.nl>
2020-06-27 21:09:14 +0200

    only animate by router animation

--- src/App.vue
+++ src/App.vue
@@ -22,14 +22,27 @@ export default {
   data() {
     return {
       direction: '',
+      popStateDetected: false,
     }
   },
   watch: {
     // watch the `$route` to determine the transition to use
     $route(to) {
-      const isListPage = to.name === 'Root' || to.name === 'City'
-      this.direction = isListPage ? 'left' : 'right'
+      // only animate by app navigation
+      if (!this.popStateDetected) {
+        const isListPage = to.name === 'Root' || to.name === 'City'
+        this.direction = isListPage ? 'left' : 'right'
+      } else {
+        this.direction = 'none'
+      }
+      this.popStateDetected = false
     },
+  },
+  mounted() {
+    // detect if user is using a back/forward button from the browser
+    window.addEventListener('popstate', () => {
+      this.popStateDetected = true
+    })
   },
 }
 </script>
