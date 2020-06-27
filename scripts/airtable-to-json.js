d5c78ebdd311d107e0a985ca7de37ff287484d90
Desmond Brand <dmnd@desmondbrand.com>
2020-06-27 16:11:32 -0700

    Sync data with Jason's spreadsheet

--- scripts/airtable-to-json.js
+++ scripts/airtable-to-json.js
@@ -4,7 +4,30 @@ const fs = require('fs')
 const Airtable = require('airtable')
 
 function extractField(s) {
-  return s?.trim() ?? null
+  return s?.trim() || null
+}
+
+// TODO(dmnd): Get rid of this stuff by enforcing consistency in AirTable...
+const badYouTubeUrls = new Set()
+const YT_REGEX = [
+  new RegExp('^(?:https://)?youtu.be/([\\w-]+)(?:\\?.*)?$'),
+  new RegExp('^https://(?:www\\.)?youtube\\.com/watch\\?v=([\\w-]+)(?:\\?.*)?$'),
+]
+
+function extractYouTubeField(s) {
+  const url = extractField(s)
+  if (url == null) {
+    return null
+  }
+
+  for (const re of YT_REGEX) {
+    let match = url.match(re)
+    if (match) {
+      return match[1]
+    }
+  }
+  badYouTubeUrls.add(url)
+  return null
 }
 
 Airtable.configure({
@@ -24,11 +47,12 @@ base('GeorgeFloyd')
       records.forEach((record) => {
         const f = record.fields
         recs.push({
-          id: extractField(f['TGD number']),
+          id: extractField(f['TGD Number']),
           city: extractField(f.City),
           state: extractField(f.State),
           tweet: extractField(f['Tweet URL']),
-          youtube: extractField(f.YouTube),
+          file: extractField(f['Video/Image Filename']),
+          youtube: extractYouTubeField(f.YouTube),
           text: extractField(f['Doucette Text']),
         })
       })
@@ -39,8 +63,19 @@ base('GeorgeFloyd')
         console.error(err)
         process.exit(1)
       }
-      const filename = 'videos.json'
-      const result = { list: recs }
-      fs.writeFileSync(filename, JSON.stringify(result, null, 2))
+      // Put "0." GCDs at the end to make diffs better
+      const list = [
+        ...recs.filter(({ id }) => !id.startsWith('0.')),
+        ...recs.filter(({ id }) => id.startsWith('0.')),
+      ]
+
+      const result = { list }
+      fs.writeFileSync('src/data/incidents.json', JSON.stringify(list, null, 2) + '\n')
+      if (badYouTubeUrls.size > 0) {
+        console.error('Ignored YouTube urls')
+        for (const url of badYouTubeUrls) {
+          console.log(url)
+        }
+      }
     },
   )
