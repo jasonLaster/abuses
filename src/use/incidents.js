d09d058be2f82bbec496a56d8af0c4fcce468b3a
Desmond Brand <dmnd@desmondbrand.com>
2020-06-27 21:14:03 -0700

    Show newest incidents first

--- src/use/incidents.js
+++ src/use/incidents.js
@@ -1,8 +1,27 @@
 import { ref, computed } from '@vue/composition-api'
 import incidents from '@/data/incidents.json'
 
+const FUTURE = '2999-12-31' // Philip J. Fry's thawing out day
+
 export default () => {
-  const list = ref(incidents.filter((incident) => incident.youtube != null))
+  const list = ref(
+    incidents
+      .filter((incident) => incident.youtube != null)
+      .sort((a, b) => {
+        // assume incidents with missing datetimes are newest
+        const adt = a.tweetDateTime ?? FUTURE
+        const bdt = b.tweetDateTime ?? FUTURE
+
+        if (adt === bdt) {
+          // when dates are missing, show newest GCD number first
+          return parseFloat(b.id) - parseFloat(a.id)
+        }
+        if (bdt < adt) {
+          return -1
+        }
+        return 1
+      }),
+  )
   const total = computed(() => list.value.length)
 
   const getIncidentByID = (ID) => {
